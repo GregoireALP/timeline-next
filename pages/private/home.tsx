1bc1d2dd96545035811a9d87386b1e560f81685b
GregoireALP <gregoire.alperovitch@gmail.com>
2021-11-29 21:07:09 +0100

    fix

--- pages/private/home.tsx
+++ pages/private/home.tsx
@@ -1,8 +1,4 @@
 import type { NextApiRequest, NextApiResponse, NextPage, NextPageContext } from "next"
-import { Document } from "react-pdf"
-import PrivateLayout from "../../components/privateLayout"
-import { checkUser } from "../../core/Middleware"
-
 const Home: NextPage = () => {
 
     return (
